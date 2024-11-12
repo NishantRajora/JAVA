f430bf4cb4636034e91e1fdac23d22b44df3f1f7
NishantRajora <nishantrajora100@gmail.com>
2024-11-12 10:09:05 +0530

    add the grpaph

--- DSAex/src/csvfile.java
+++ DSAex/src/csvfile.java
@@ -2,11 +2,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
+
+
 
 public class csvfile {
+
+    
     public static void main(String[] args) {
         int max_row = 0;
         int max_col = 0;
+        
 
         try {
             File file = new File("D:\\Java\\DSAex\\src\\sample.csv"); 
@@ -29,6 +35,7 @@ public class csvfile {
         }
 
         int[][] matrix = new int[max_row][max_col];
+        
 
         try {
             File file = new File("D:\\Java\\DSAex\\src\\sample.csv"); 
@@ -44,19 +51,45 @@ public class csvfile {
                 int data = Integer.parseInt(values[2].trim());
 
                 matrix[row][col] = data;
+                
+
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
             return;
         }
-        System.out.println(max_row);
-        System.out.println(max_col);
+        //System.out.println(max_row);
+        //System.out.println(max_col);
 
         for (int[] row : matrix) {
             for (int value : row) {
                 System.out.print(value + " ");
+
             }
             System.out.println();
         }
+
+
+        Stack<Integer> s = new Stack<>();
+        boolean[] visited = new boolean[max_row];
+
+        for (int i = 0; i < max_row; i++) {
+            if (!visited[i]) {
+                s.push(i);
+                visited[i] =true;
+        
+                while (!s.isEmpty()) {
+                    int node = s.pop();
+                    System.out.print((node + 1) + " "); 
+
+                    for (int j = 0; j < matrix[node].length; j++) {
+                        if (matrix[node][j] != 0 && !visited[j]) {
+                            s.push(j);
+                            visited[j] = true;
+                        }
+                    }
+                }
+            }      
+        }    
     }
 }
