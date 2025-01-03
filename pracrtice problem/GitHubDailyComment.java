86e539076010245ba731e2eb2ae867136b338114
NishantRajora <nishantrajora100@gmail.com>
2025-01-03 22:59:49 +0530

    cv

--- pracrtice problem/GitHubDailyComment.java
+++ pracrtice problem/GitHubDailyComment.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 
 public class GitHubDailyComment {
     public static void main(String[] args) {
-        String filePath = "D:\\Java\\pracrtice problem"; // Replace with the path to your file
+        // Local file path to update
+        String filePath = "D:\\Java\\pracrtice problem\\GitHubDailyComment.java";
+
         try {
             // Add 10 comments to the file
             addComments(filePath);
 
             // Run Git commands to commit and push changes
-            executeCommand("git add " + filePath);
-            executeCommand("git commit -m \"Daily comment update\"");
-            executeCommand("git push");
+            executeCommand("git -C D:\\Java\\pracrtice problem add GitHubDailyComment.java");
+            executeCommand("git -C D:\\Java\\pracrtice problem commit -m \"Daily comment update\"");
+            executeCommand("git -C D:\\Java\\pracrtice problem push");
 
             System.out.println("Comments added and changes pushed to GitHub.");
         } catch (IOException | InterruptedException e) {
@@ -23,7 +25,7 @@ public class GitHubDailyComment {
     private static void addComments(String filePath) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
             for (int i = 1; i <= 10; i++) {
-                writer.write("// Comment " + i + ": This is a changable comment.\n");
+                writer.write("// Comment " + i + ": This is a changeable comment.\n");
             }
             System.out.println("10 comments added to the file.");
         }
@@ -41,3 +43,23 @@ public class GitHubDailyComment {
         }
     }
 }
+// Comment 1: This is a changeable comment.
+// Comment 2: This is a changeable comment.
+// Comment 3: This is a changeable comment.
+// Comment 4: This is a changeable comment.
+// Comment 5: This is a changeable comment.
+// Comment 6: This is a changeable comment.
+// Comment 7: This is a changeable comment.
+// Comment 8: This is a changeable comment.
+// Comment 9: This is a changeable comment.
+// Comment 10: This is a changeable comment.
+// Comment 1: This is a changeable comment.
+// Comment 2: This is a changeable comment.
+// Comment 3: This is a changeable comment.
+// Comment 4: This is a changeable comment.
+// Comment 5: This is a changeable comment.
+// Comment 6: This is a changeable comment.
+// Comment 7: This is a changeable comment.
+// Comment 8: This is a changeable comment.
+// Comment 9: This is a changeable comment.
+// Comment 10: This is a changeable comment.
